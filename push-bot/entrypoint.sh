f61f3da412538c466d2ed292888b0aaa13b2a2c4
Kavuti <christian.cavuti@gmail.com>
2023-06-07 15:37:49 +0200

    feat: moved in the push-bot action

---
+++ push-bot/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -l
+
+token=$1
+branch_name=$2
+bot_name=$3
+
+REPO_REF="https://${bot_name}:${token}@github.com/${GITHUB_REPOSITORY}.git"
+git config http.sslVerify false
+git config user.name "${bot_name}"
+git config user.email "${bot_name}@users.noreply.github.com"
+git remote add upstream "${REPO_REF}"
+git branch --verbose
+
+git checkout ${branch_name}
+git add -A
+timestamp=$(date -u)
+git commit -m "Bot published ${GITHUB_SHA} at ${timestamp}" || true
+git pull --rebase upstream ${branch_name}
+git push upstream ${branch_name}
