7ee93bf43a998b5a20c3dbe324847e77ada860ad
Marco Carlino <marcocharlie01@gmail.com>
2023-06-15 17:41:02 +0200

    add dags path

--- deploy-airflow-dags/deploy_dags.sh
+++ deploy-airflow-dags/deploy_dags.sh
@@ -7,11 +7,10 @@ then
 else
     ZIP_FILENAME=$PROJECT'_'$VERSION.zip
 fi
-cd dags
+cd $AIRFLOW_DAGS_PATH
 touch __init__.py
 echo "__version__='$VERSION'" > _version.py
 echo "__project__='$PROJECT'" > _project.py
 echo "__versioned__='$VERSIONED_DEPLOY'" > _is_versioned.py
 zip  -r $ZIP_FILENAME *
-cd ..
-cp dags/$ZIP_FILENAME /mnt/efs/airflow-data-dags
+cp $AIRFLOW_DAGS_PATH/$ZIP_FILENAME /mnt/efs/airflow-data-dags
