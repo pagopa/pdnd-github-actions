39d4ae95f9de562dce0ddfaf367c7f6ee555e7df
Marco Carlino <marcocharlie01@gmail.com>
2023-06-16 15:10:54 +0200

    test composite action

--- deploy-airflow-dags/deploy_dags.sh
+++ deploy-airflow-dags/deploy_dags.sh
@@ -13,5 +13,5 @@ echo "__version__='$VERSION'" > _version.py
 echo "__project__='$PROJECT'" > _project.py
 echo "__versioned__='$VERSIONED_DEPLOY'" > _is_versioned.py
 zip  -r $ZIP_FILENAME *
-ls /mnt
+ls /mnt/efs
 cp $ZIP_FILENAME $AIRFLOW_DAGS_VOLUME_PATH
