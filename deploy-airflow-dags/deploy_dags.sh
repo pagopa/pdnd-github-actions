6981d4d6c11140cd306ed4912d41cc46cc16080b
Marco Carlino <44462518+marcocharlie@users.noreply.github.com>
2023-06-19 17:18:02 +0200

    [PA-349] Add airflow deploy action (#5)
    * add deploy airflow dags
    
    * add params
    
    * fix path runner
    
    * fix metadata path
    
    * fix deploy script path
    
    * fix deploy script
    
    * add dags path
    
    * add zip
    
    * Update README.md
    
    * fix zip path
    
    * add airflow_dags_volume_path
    
    * test ls
    
    * fix typo
    
    * test ls
    
    * test composite action
    
    * add shell param
    
    * typo
    
    * add action path
    
    * remove runner from path
    
    * add cd
    
    * remove docker and old action
    
    * remove ls
    
    * add success log
    
    * fix readme
    
    * Delete requirements.txt
    
    * fix: added runner volumes docs
    
    * enrich docs
    
    * enrich action description
    
    * enrich docs
    
    * rename airflow_dags_folder_path
    
    * fix typo
    
    * Update README.md
    
    * Update README.md
    
    * enrich example
    
    * add example output image
    
    * add check dags folder
    
    * Update README.md
    
    * docs suggestrions
    
    * fix dags folder check
    
    * fix param description
    
    * make airflow_dags_folder_path not required
    
    * fix shell
    
    * fix docs
    
    ---------
    
    Co-authored-by: Kavuti <christian.cavuti@gmail.com>

---
+++ deploy-airflow-dags/deploy_dags.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+source ./deploy_metadata.env
+echo to version deploy : $VERSIONED_DEPLOY
+ if [ "$VERSIONED_DEPLOY" = false ]
+then
+    ZIP_FILENAME=$PROJECT.zip
+else
+    ZIP_FILENAME=$PROJECT'_'$VERSION.zip
+fi
+cd $AIRFLOW_DAGS_FOLDER_PATH
+touch __init__.py
+echo "__version__='$VERSION'" > _version.py
+echo "__project__='$PROJECT'" > _project.py
+echo "__versioned__='$VERSIONED_DEPLOY'" > _is_versioned.py
+zip  -r $ZIP_FILENAME *
+cp $ZIP_FILENAME $AIRFLOW_DAGS_VOLUME_PATH
+echo "Deploy to Airflow succeeded!"
