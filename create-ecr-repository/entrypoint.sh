e164d503a6a77d362398864a02a2043936fc6d52
mronci <112618063+mronci@users.noreply.github.com>
2023-10-13 17:25:54 +0200

    fix

--- create-ecr-repository/entrypoint.sh
+++ create-ecr-repository/entrypoint.sh
@@ -6,6 +6,7 @@ export tags=$INPUT_TAGS
 export tagStatus=$INPUT_TAGSTATUS
 export countType=$INPUT_COUNTTYPE
 export countNumber=$INPUT_COUNTNUMBER
-cat $AWS_WEB_IDENTITY_TOKEN > /tmp/eks-token
+
+echo $AWS_WEB_IDENTITY_TOKEN > /tmp/eks-token
 export AWS_WEB_IDENTITY_TOKEN_FILE=/tmp/eks-token
 python /main.py
