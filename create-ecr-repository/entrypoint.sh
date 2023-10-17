5875ddd80a05e7f0abf301763dec414f370d62e8
mronci <112618063+mronci@users.noreply.github.com>
2023-10-17 15:12:11 +0200

    testing with composite

--- create-ecr-repository/entrypoint.sh
+++ create-ecr-repository/entrypoint.sh
@@ -7,9 +7,4 @@ export tagStatus=$INPUT_TAGSTATUS
 export countType=$INPUT_COUNTTYPE
 export countNumber=$INPUT_COUNTNUMBER
 
-if [ -n "$AWS_WEB_IDENTITY_TOKEN" ]; then
-  echo $AWS_WEB_IDENTITY_TOKEN > /tmp/eks-token
-  export AWS_WEB_IDENTITY_TOKEN_FILE=/tmp/eks-token
-fi
-
 python /main.py
